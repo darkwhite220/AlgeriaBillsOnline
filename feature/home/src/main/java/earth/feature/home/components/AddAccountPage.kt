0778114920a1c0a9d57d9fd7f54985629346d9bc
darkwhite220 <53045980+darkwhite220@users.noreply.github.com>
2024-01-24 21:40:00 +0100

    Add HomeScreen AddAccountPage bottomIndicator space

--- feature/home/src/main/java/earth/feature/home/components/AddAccountPage.kt
+++ feature/home/src/main/java/earth/feature/home/components/AddAccountPage.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,6 +27,7 @@ fun AddAccountPage(onHomeEvent: (HomeEvent) -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .padding(horizontal = largeDp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -59,5 +62,8 @@ fun AddAccountPage(onHomeEvent: (HomeEvent) -> Unit) {
             textId = R.string.sign_in,
             onClick = { onHomeEvent(HomeEvent.OnSignInClick) }
         )
+        
+        // Bottom indicators padding
+        MyHeightSpacer(largeDp)
     }
 }
