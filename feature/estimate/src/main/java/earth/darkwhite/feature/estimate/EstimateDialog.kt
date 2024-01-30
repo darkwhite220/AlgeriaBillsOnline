4043cf95a5eefb82a62db1f1148324d8ad02d62f
darkwhite220 <53045980+darkwhite220@users.noreply.github.com>
2024-01-30 02:29:09 +0100

    Add closs button in estimate dialog detail

--- feature/estimate/src/main/java/earth/darkwhite/feature/estimate/EstimateDialog.kt
+++ feature/estimate/src/main/java/earth/darkwhite/feature/estimate/EstimateDialog.kt
@@ -1,7 +1,9 @@
 package earth.darkwhite.feature.estimate
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -10,6 +12,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -75,6 +78,14 @@ fun EstimateDetailDialog(
                     text = stringResource(R.string.select_details_note),
                     style = MaterialTheme.typography.bodySmall
                 )
+                Row(
+                    horizontalArrangement = Arrangement.End,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    TextButton(onClick = onDismiss) {
+                        Text(text = stringResource(id = earth.core.designsystem.R.string.close))
+                    }
+                }
             }
         }
     }
