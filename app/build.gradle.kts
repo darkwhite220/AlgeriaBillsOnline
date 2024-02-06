6afe47d9393b9c34c4a73f141acc6f71b20dba83
darkwhite220 <53045980+darkwhite220@users.noreply.github.com>
2024-02-06 19:07:06 +0100

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "earth.darkwhite.algeriabills"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
         
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
